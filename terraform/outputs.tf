670c04c21dcb25927bda8ce754cb509b5bb4f636
jhante charles <jhante.charles@gmail.com>
2025-10-17 14:58:27 -0400

    adding pipeline and base terraform files

---
+++ terraform/outputs.tf
@@ -0,0 +1,32 @@
+
+# Deployment Config
+
+resourcegroup_name = "adoLZ"
+
+location = "East US 2"
+
+tags = {
+  "Environment" = "Lab"
+  "Owner"       = "lab@wahtever"
+}
+
+vnet_name = "lab-vnet"
+
+vnet_address_space = ["10.211.0.0/16"]
+
+subnets = {
+  Subnet_1 = {
+    name             = "subnet_1"
+    address_prefixes = ["10.211.1.0/24"]
+  }
+  Subnet_2 = {
+    name             = "subnet_2"
+    address_prefixes = ["10.211.2.0/24"]
+  }
+  bastion_subnet = {
+    name             = "AzureBastionSubnet"
+    address_prefixes = ["10.211.250.0/24"]
+  }
+}
+
+bastionhost_name = "BastionHost"
